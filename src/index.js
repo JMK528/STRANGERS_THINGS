e84b53a415ad05f7c53f0971ea5713d2bf13b156
Justin Kerr <justinmk1@comcast.net>
2022-09-02 16:45:17 -0600

    Adding in the Login function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter, Routes, useNavigate } from 'react-router-dom';
+import './style.css';
+import {
+  Navbar,
+  Posts,
+  Profile,
+  Home,
+  Register
+} from './components';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {
+  getPosts
+} from './api';
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [token, setToken] = useState('');
+  const navigate = useNavigate();
+  console.log(navigate)
+  
+  console.log(token)
+  
+  async function fetchPosts() {
+    const results = await getPosts()
+    setPosts(results.data.posts);
+  }
+  
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+  
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/posts' element={<Posts posts={posts} />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route 
+          path='/register'
+          element={<Register
+          setToken={ setToken } 
+          token={token}
+          navigate={navigate}/>} />
+      </Routes>
+    </div>
+  )
+}
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+/*
+Login
+Registeration
+Posts
+Profile
+Navbar
+AddPost
+
+*/
